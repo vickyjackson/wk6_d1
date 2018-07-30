0e4d8ace48234695e49b915a9d8386463b8278a5
Vicky Jackson <vicky@digitalstuff.net>
2018-07-30 22:26:22 +0100

    Changed Runner comments to better reflect whats happening

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -12,18 +12,18 @@ public class Runner {
         // create new printer object with values
         Printer printer = new Printer(100);
 
-        // print out the result of calling the Calculator methods on calculator object
+        // call the Calculator methods on calculator object
         calculator.add();
         calculator.subtract();
         calculator.multiply();
         calculator.divide();
 
-        // print out the result of calling the WaterBottle methods on waterbottle object
+        // call the WaterBottle methods on waterbottle object
         waterbottle.drink();
         waterbottle.empty();
         waterbottle.fill();
 
-        // print out the result of calling the Printer methods on printer object
+        // call the Printer methods on printer object
         printer.print(10, 4);
         printer.refill();
 
